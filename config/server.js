fdcedcb1035bd8a9fdf5d6ce7bd1370d7a8e33fe
Cefalu Wave <luke.evan.graham@gmail.com>
2022-04-07 21:18:29 -0700

    creating test content for home

--- config/server.js
+++ config/server.js
@@ -2,6 +2,8 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    // keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS', ['7a006609dcdecc5b', '424308b58181ae7f'])
+
   },
 });
